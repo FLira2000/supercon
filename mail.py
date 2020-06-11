230a4fcf91f62a053c8f0cfa139774f3829c9446
Fábio Lira <fabioliradev@gmail.com>
2020-06-11 02:52:15 -0300

    creating mail layer

---
+++ mail.py
@@ -0,0 +1,37 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.application import MIMEApplication
+from email import encoders
+from os.path import basename
+
+MAIL_CONFIG = { "email": "data.supercon@gmail.com", "password": "rnzizqregtssljji" }
+targetEmail = "fabioliradev@gmail.com"
+ccEmail = "josiasdsj1@gmail.com"
+
+def mailSender(attachmentName, bodyMessage):
+
+    #sending results over email
+    msg = MIMEMultipart()
+    msg['From'] = "data.supercon@gmail.com"
+    msg['To'] = targetEmail
+    msg['Subject'] = "Prediction data"
+    msg['Cc'] = ', '.join([ccEmail])
+
+    body = bodyMessage
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    part = MIMEApplication(open(attachmentName, "rb").read(), Name=basename(attachmentName))
+    part['Content-Disposition'] = 'attachment; filename="%s"' % basename(attachmentName)
+
+    msg.attach(part)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(MAIL_CONFIG["email"], MAIL_CONFIG["password"])
+    s.send_message(msg)
+    s.quit()
+
+    return 
