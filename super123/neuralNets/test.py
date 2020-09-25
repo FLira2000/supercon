1c27cffc27a8ade0b83c6ec6861965341f53c870
Fábio Lira <fabioliradev@gmail.com>
2020-09-25 15:47:37 -0300

    only-python functions

---
+++ super123/neuralNets/test.py
@@ -0,0 +1,13 @@
+from NN_logisticActiv_doubleLayer import neural_logistic_2layer as nnL2
+from NN_logisticActiv_singleLayer import neural_logistic_1layer as nnL1
+from NN_reluActiv_doubleLayer import neural_relu_2layer as nnR2
+from NN_reluActiv_singleLayer import neural_relu_1layer as nnR1
+from NN_tahnActiv_doubleLayer import neural_tahn_2layer as nnT2
+from NN_tahnActiv_singleLayer import neural_tahn_1layer as nnT1
+
+print(nnL2)
+print(nnL1)
+print(nnR2)
+print(nnR1)
+print(nnT2)
+print(nnT1)
