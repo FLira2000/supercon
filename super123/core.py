0b0182f12f7a3b600f8751bc31593efa3a4f07f9
Fábio L <fabioliradev@gmail.com>
2020-10-11 13:41:59 -0300

    fixing file calling

--- super123/core.py
+++ super123/core.py
@@ -4,19 +4,19 @@ from matplotlib import pyplot as plt
 from sklearn.impute import SimpleImputer
 import os
 from sklearn.model_selection import train_test_split
-from super123.neuralNets.openfile import fileWrapper
-from super123.neuralNets.NN_logisticActiv_doubleLayer import neural_logistic_2layer
-from super123.neuralNets.NN_logisticActiv_singleLayer import neural_logistic_1layer
-from super123.neuralNets.NN_reluActiv_doubleLayer import neural_relu_2layer
-from super123.neuralNets.NN_reluActiv_singleLayer import neural_relu_1layer
-from super123.neuralNets.NN_tahnActiv_doubleLayer import neural_tahn_2layer
-from super123.neuralNets.NN_tahnActiv_singleLayer import neural_tahn_1layer
-from super123.nn_test_iterator import NNtest_iterator
+from neuralNets.NN_logisticActiv_doubleLayer import neural_logistic_2layer
+from neuralNets.NN_logisticActiv_singleLayer import neural_logistic_1layer
+from neuralNets.NN_reluActiv_doubleLayer import neural_relu_2layer
+from neuralNets.NN_reluActiv_singleLayer import neural_relu_1layer
+from neuralNets.NN_tahnActiv_doubleLayer import neural_tahn_2layer
+from neuralNets.NN_tahnActiv_singleLayer import neural_tahn_1layer
+from nn_test_iterator import NNtest_iterator
 
-df = pd.read_csv(fileWrapper())
+df = pd.read_csv(f"./databases/super123Database_final.csv")
 df = df.drop("Unnamed: 0", axis=1).drop("Unnamed: 0.1", axis=1)
 
 y = df['critical_temp']
+
 x = df.select_dtypes(exclude=['object']).drop('critical_temp', axis=1)
 
 train_X, test_X, train_y, test_y = train_test_split(x.values, y.values, test_size=0.2)
--- super123/neuralNets/openfile.py
+++ super123/neuralNets/openfile.py
@@ -1,2 +1,2 @@
 def fileWrapper():
-    return f"./super123/databases/super123Database_final.csv"
+    return f"../databases/super123Database_final.csv"
