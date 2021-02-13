c3d525bc76c713d8fb16ba703227286fc2aa4b72
FLira2000 <fabioliradev@gmail.com>
2021-02-13 03:22:28 -0300

    isolating layers and modules

---
+++ application/makeup.py
@@ -0,0 +1,11 @@
+from thermo import nested_formula_parser as parser
+
+def makeup(element):
+    print(element)
+    print(type(element))
+
+    chem_dict = parser(element)
+
+    return chem_dict
+
+print(makeup('H2O'))
